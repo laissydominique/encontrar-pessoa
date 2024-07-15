7a3ca71172e1d9699946bf130b3d5709efc61f0d
Laissy <laissydominique@gmail.com>
2024-07-15 15:16:16 -0300

    ids

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,44 @@ const info = ref({
 
 async function teste() {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${info.value.id}`
+    `https://dummyapi.online/api/social-profiles/${info.value.id}`
   );
   const data = await response.json();
   console.log(data);
 
   info.value.dados = {
-    nome: data.name,
-    tel: data.phone,
+    nome: data.fullName,
     email: data.email,
     user: data.username,
+    trabalho: data.work,
+    cidade: data.location,
   };
 
-  if (!data.id) {
+  if (!data.userId) {
     info.value.erro = true;
+  }else{
+    info.value.erro = false;
+
   }
 }
 </script>
 
 <template>
-  <input v-model="info.id" type="text" />
+  <input  v-model="info.id" type="text" placeholder="Digite ID aqui" />
   <button @click="teste"></button>
 
   <p class="nome">{{ info.dados.nome }}</p>
-  <p class="tel">{{ info.dados.tel }}</p>
   <p class="email">{{ info.dados.email }}</p>
   <p class="user">{{ info.dados.user }}</p>
+  <p class="trabalho">{{ info.dados.trabalho }}</p>
+  <p class="cidade">{{ info.dados.cidade }}</p>
+
+
+  
 
   <p v-if="info.erro" class="erro">Id não encontrado</p>
 </template>
 
 <style scoped>
-button {
-  width: 70px;
-  height: 20px;
-}
+
 </style>
